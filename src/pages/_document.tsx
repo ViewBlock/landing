27950af311de8630b0d80c519e5b228c91b6bf67
John Letey <johnletey@gmail.com>
2021-03-01 17:36:59 +0000

    feat: seo and opengraph

---
+++ src/pages/_document.tsx
@@ -0,0 +1,54 @@
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+
+class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Primary Meta Tags */}
+          <title>KYVE</title>
+          <meta name="title" content="KYVE" />
+          <meta
+            name="description"
+            content="The unified archive for blockchains."
+          />
+
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://kyve.network/" />
+          <meta property="og:title" content="KYVE" />
+          <meta
+            property="og:description"
+            content="The unified archive for blockchains."
+          />
+
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://kyve.network/" />
+          <meta property="twitter:title" content="KYVE" />
+          <meta
+            property="twitter:description"
+            content="The unified archive for blockchains."
+          />
+
+          <link
+            rel="icon"
+            href="https://kyve.network/favicon.svg"
+            type="image/svg"
+          />
+          <meta property="og:image" content="https://kyve.network/og.png" />
+          <meta
+            property="twitter:image"
+            content="https://kyve.network/og.png"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default Document;
