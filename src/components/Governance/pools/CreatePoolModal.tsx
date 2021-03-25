2ee44965c7d7eeb136aee00781c1deaa7c17180b
Fabian Riewe <fabianriewe00@gmail.com>
2021-03-25 11:04:39 +0100

    feat: added createPool function

--- src/components/Governance/pools/CreatePoolModal.tsx
+++ src/components/Governance/pools/CreatePoolModal.tsx
@@ -1,12 +1,61 @@
-import {Modal, useModal} from "@geist-ui/react";
+import {
+  Input,
+  Modal,
+  Textarea,
+  useInput,
+  useModal,
+  Text,
+  Select,
+  useToasts,
+} from "@geist-ui/react";
 import {forwardRef, useImperativeHandle, useState} from "react";
 
+import {interactWrite} from "smartweave";
+
+import {arweave} from "../../../extensions";
+
 const CreatePoolModal = forwardRef((props, ref) => {
-  const { setVisible, bindings } = useModal();
+  const {setVisible, bindings} = useModal();
 
   const [loading, setLoading] = useState(false);
 
-  const createPool = async () => {};
+  const [toasts, setToast] = useToasts();
+
+  // declare inputs
+  const {state: pool, bindings: bindingsPool} = useInput("");
+  // TODO SHOULD BE SELECT
+  const {
+    state: architecture,
+    setState: setArchitecture,
+  } = useInput("Avalanche");
+  const {state: config, bindings: bindingsConfig} = useInput("{}");
+  const {state: bundleSize, bindings: bindingsBundleSize} = useInput("20");
+  const {state: uploader, bindings: bindingsUploader} = useInput(
+    "3dX8Cnz3N64nKt2EKmWpKL1EbErFP3RFjxSDyQHQrkI"
+  );
+  const {state: archiveRate, bindings: bindingsArchiveRate} = useInput("1");
+  const {state: validatorRate, bindings: bindingsValidatorRate} = useInput(
+    "1"
+  );
+
+  const contractId = "v2p-0OhAxDCCMLjQ8e_6_YhT3Tfw2uUAbIQ3PXRtjr4";
+
+  const createPool = async () => {
+    const input = {
+      function: "createPool",
+      pool: pool,
+      architecture: architecture,
+      config: config,
+      bundleSize: bundleSize,
+      uploader: uploader,
+      archiveRate: archiveRate,
+      validatorRate: validatorRate,
+    };
+    console.log(input);
+    const state = await interactWrite(arweave, undefined, contractId, input);
+    console.log(state);
+    setToast({text: 'Pool successfully created', type: 'success'})
+  };
 
   useImperativeHandle(ref, () => ({
     open() {
@@ -18,7 +67,39 @@ const CreatePoolModal = forwardRef((props, ref) => {
     <>
       <Modal {...bindings}>
         <Modal.Title>Create Pool</Modal.Title>
-        <Modal.Content>Create a pool here</Modal.Content>
+        <Modal.Content>
+          <Input {...bindingsPool} width={"100%"}>
+            Poolname
+          </Input>
+          <Text>Architecture</Text>
+          <Select
+            placeholder="Architecture"
+            width={"100%"}
+            onChange={(value) => {
+              // @ts-ignore
+              setArchitecture(value);
+            }}
+          >
+            <Select.Option value="Avalanche">Avalanche</Select.Option>
+            <Select.Option value="Cosmos">Cosmos</Select.Option>
+            <Select.Option value="Polkadot">Polkadot</Select.Option>
+            <Select.Option value="Solana">Solana</Select.Option>
+          </Select>
+          <Text>Config</Text>
+          <Textarea {...bindingsConfig} width={"100%"}/>
+          <Input {...bindingsBundleSize} width={"100%"}>
+            Bundle size
+          </Input>
+          <Input {...bindingsUploader} width={"100%"} disabled={true}>
+            Uploader
+          </Input>
+          <Input {...bindingsArchiveRate} width={"100%"}>
+            Archive rate
+          </Input>
+          <Input {...bindingsValidatorRate} width={"100%"}>
+            Validator rate
+          </Input>
+        </Modal.Content>
         <Modal.Action passive onClick={() => setVisible(false)}>
           Cancel
         </Modal.Action>
